eded483b47fcae24986c362c96e9f17d8c7f30ec
Diogo Sales <diogodobu@gmail.com>
2023-05-28 16:12:31 -0300

    Editado navbar

--- components/Cabecalho.jsx
+++ components/Cabecalho.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Container, Image, Nav, Navbar } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const Cabecalho = () => {
@@ -7,7 +7,12 @@ const Cabecalho = () => {
     <>
       <Navbar variant="dark" style={{ backgroundColor: "green" }}>
         <Container>
-          <Navbar.Brand href="/">Início</Navbar.Brand>
+          <Navbar.Brand href="/">
+            <Image
+              src="https://play-lh.googleusercontent.com/ygJ_iSQbDxlZt_LOvBjjouzUq6B-U1YCByeHteK2tSoqWm1zEDQYH14SYfcoi3WeVjA"
+              style={{ height: 60, width: 60 }}
+            />
+          </Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="/transformacoes">Transformações</Nav.Link>
             <Nav.Link href="/viloes">Vilões</Nav.Link>
