b1067e0f6ea5fd2be0dd62715a861856ea9ebfc3
Diogo Sales <diogodobu@gmail.com>
2023-06-06 23:13:32 -0300

    Primeira tela com firebase

--- pages/desenhos/index.jsx
+++ pages/desenhos/index.jsx
@@ -1,12 +1,25 @@
 import React, { useEffect, useState } from "react";
 import Pagina from "../../components/Pagina";
+import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import data from "@/services/data";
 import { Row, Col, Card, Button } from "react-bootstrap";
 import MeuCard from "@/components/MeuCard";
 
 const index = () => {
-  const desenhos = data.desenhos;
+  // const desenhos = data.desenhos;
+
+  const [desenhos, setDesenhos] = useState([]);
+
+  useEffect(() => {
+    getAll();
+  }, []);
+
+  function getAll() {
+    axios.get("/api/desenhos").then((res) => {
+      setDesenhos(res.data);
+    });
+  }
 
   return (
     <Pagina titulo="Desenhos">
