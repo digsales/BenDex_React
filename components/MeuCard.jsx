d2e85dbf0baf443eee7c27f04e3e8e01b33b6c81
Diogo Sales <diogodobu@gmail.com>
2023-06-13 20:33:01 -0300

    Alterado MeuCard

--- components/MeuCard.jsx
+++ components/MeuCard.jsx
@@ -2,7 +2,24 @@ import React from "react";
 import { Card } from "react-bootstrap";
 
 const MeuCard = (props) => {
-  return <Card className="card border-dark mb-3">{props.children}</Card>;
+  // return <Card className="card border-dark mb-3">{props.children}</Card>;
+  return (
+    <Card
+      border="success"
+      style={{
+        boxShadow: "0 17px 10px rgba(0, 0, 0, 0.3)",
+        transition: "transform 0.3s ease",
+      }}
+      onMouseEnter={(e) => {
+        e.currentTarget.style.transform = "scale(1.05)";
+      }}
+      onMouseLeave={(e) => {
+        e.currentTarget.style.transform = "scale(1)";
+      }}
+    >
+      {props.children}
+    </Card>
+  );
 };
 
 export default MeuCard;
