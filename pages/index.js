28a884bd0ea34e4483847fca58493816f6604fc9
Diogo Sales <diogodobu@gmail.com>
2023-06-15 21:35:35 -0300

    Adicionando tela de carregamento

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,42 @@
-import Pagina from "@/components/Pagina";
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import PuffLoader from "react-spinners/PuffLoader";
+import { useRouter } from "next/router";
 
 const index = () => {
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+      router.push("/transformacoes");
+    }, 3000);
+  }, []);
   return (
-    <Pagina titulo="Ben Dex">
-      <h3>A enciclopédia do seu desenho favorito.</h3>
-    </Pagina>
+    <div
+      style={{
+        backgroundColor: "#1C1C1C",
+        textAlign: "center",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      {loading ? (
+        <PuffLoader color={"#17583B"} loading={loading} size={300} />
+      ) : (
+        <></>
+      )}
+      {loading && (
+        <img
+          src="https://i.pinimg.com/originals/59/41/73/594173036931a34e7952732d76b000c3.png"
+          alt="Loading"
+          style={{ width: 100, height: 100, position: "absolute" }}
+        />
+      )}
+    </div>
   );
 };
 
