70950feef0b5244f33002171b329c37bef36c230
Diogo Sales <diogodobu@gmail.com>
2023-06-15 21:08:48 -0300

    Editando apresentação do [id]

--- pages/viloes/[id].jsx
+++ pages/viloes/[id].jsx
@@ -4,6 +4,7 @@ import data from "@/services/data";
 import { Card, Col, Row } from "react-bootstrap";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import MinhaArea from "@/components/MinhaArea";
 
 const Detalhes = ({ index }) => {
   // const vilao = data.viloes;
@@ -22,37 +23,39 @@ const Detalhes = ({ index }) => {
 
   return (
     <Pagina titulo={inimigo.nome}>
-      <Row style={{ marginBottom: 50 }}>
-        <Col md={3}>
-          <Card.Img variant="top" src={inimigo.imagem} />
-        </Col>
+      <MinhaArea>
+        <Row style={{ marginBottom: 50 }}>
+          <Col md={3}>
+            <Card.Img variant="top" src={inimigo.imagem} />
+          </Col>
 
-        <Col md={9}>
-          <p>
-            <strong style={{ fontSize: 18 }}>Raça: </strong>
-            <br />
-            {inimigo.raca}
-          </p>
-          <p>
-            <strong style={{ fontSize: 18 }}>Habilidades: </strong>
-            <br />
-            {inimigo.poderes}
-          </p>
-          <div>
-            <strong style={{ fontSize: 18 }}>Aparições: </strong>
-            <ul>
-              {inimigo.desenho?.map((item) => (
-                <li>{item}</li>
-              ))}
-            </ul>
-          </div>
-        </Col>
-      </Row>
-      <p>
-        <strong style={{ fontSize: 18 }}>Planos: </strong>
-        <br />
-        {inimigo.plano}
-      </p>
+          <Col md={9}>
+            <p>
+              <strong style={{ fontSize: 18 }}>Raça: </strong>
+              <br />
+              {inimigo.raca}
+            </p>
+            <p>
+              <strong style={{ fontSize: 18 }}>Habilidades: </strong>
+              <br />
+              {inimigo.poderes}
+            </p>
+            <div>
+              <strong style={{ fontSize: 18 }}>Aparições: </strong>
+              <ul>
+                {inimigo.desenho?.map((item) => (
+                  <li>{item}</li>
+                ))}
+              </ul>
+            </div>
+          </Col>
+        </Row>
+        <p>
+          <strong style={{ fontSize: 18 }}>Planos: </strong>
+          <br />
+          {inimigo.plano}
+        </p>
+      </MinhaArea>
     </Pagina>
   );
 };
