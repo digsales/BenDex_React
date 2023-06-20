111a8c10625b5091a96b4305ccaf14167b67248e
Diogo Sales <diogodobu@gmail.com>
2023-06-20 20:59:33 -0300

    Editado titulo

--- pages/desenhos/form.jsx
+++ pages/desenhos/form.jsx
@@ -35,7 +35,7 @@ const form = () => {
   }
 
   return (
-    <Pagina titulo="Novo Predador">
+    <Pagina titulo="Novo Desenho">
       <Form>
         <Form.Group className="mb-3" controlId="nome">
           <Form.Label>Nome: </Form.Label>
