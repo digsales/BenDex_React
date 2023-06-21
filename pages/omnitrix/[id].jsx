a53c1ebbfe5de2ea48a0cb5e00b6cc525f3627d6
Diogo Sales <diogodobu@gmail.com>
2023-06-21 20:02:18 -0300

    Adicionado editar

---
+++ pages/omnitrix/[id].jsx
@@ -0,0 +1,91 @@
+import Pagina from "@/components/Pagina";
+import React, { useState, useEffect } from "react";
+import { Button, Col, Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import data from "@/services/data";
+import axios from "axios";
+import { FaSave, FaTimes } from "react-icons/fa";
+import omnitrixValidator from "@/validators/omnitrixValidator";
+
+const form = () => {
+  const { push, query } = useRouter();
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm();
+
+  const [omnitrix, setOmnitrix] = useState([]);
+
+  useEffect(() => {
+    if (query.id) {
+      axios.get(`/api/omnitrix/${query.id}`).then((res) => {
+        const omni = res.data;
+
+        for (let atributo in omni) {
+          setValue(atributo, omni[atributo]);
+        }
+      });
+    }
+  }, [query.id]);
+
+  function salvar(dados) {
+    axios.put(`/api/omnitrix/${query.id}`, dados);
+    push("/omnitrix");
+  }
+
+  return (
+    <Pagina titulo="Novo Desenho">
+      <Form>
+        <Form.Group className="mb-3" controlId="nome">
+          <Form.Label>Nome: </Form.Label>
+          <Form.Control
+            type="text"
+            {...register("nome", omnitrixValidator.nome)}
+          />
+          {errors.nome && (
+            <Form.Text className="text-danger">{errors.nome.message}</Form.Text>
+          )}
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="imagem">
+          <Form.Label>Imagem: </Form.Label>
+          <Form.Control
+            type="text"
+            {...register("imagem", { required: true })}
+          />
+          {errors.imagem && (
+            <Form.Text className="text-danger">Campo obrigatório.</Form.Text>
+          )}
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="detalhes">
+          <Form.Label>Detalhes: </Form.Label>
+          <Form.Control
+            type="text"
+            {...register("detalhes", omnitrixValidator.detalhes)}
+          />
+          {errors.detalhes && (
+            <Form.Text className="text-danger">
+              {errors.detalhes.message}
+            </Form.Text>
+          )}
+        </Form.Group>
+
+        <div className="text-center">
+          <Button variant="success" onClick={handleSubmit(salvar)}>
+            <FaSave color="white" size={14} /> Salvar
+          </Button>
+          <Link className="ms-2 btn btn-danger" href="/omnitrix">
+            <FaTimes color="white" size={14} /> Cancelar
+          </Link>
+        </div>
+      </Form>
+    </Pagina>
+  );
+};
+
+export default form;
